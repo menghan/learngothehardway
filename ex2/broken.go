f325e14b4f0d13d77fa46032d7b3118c5607e81d
Menghan <menghan412@gmail.com>
2013-07-25 00:19:32 +0800

    ex2

---
+++ ex2/broken.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var foo = "hello, world"
+	fmt.Println("foo: ", foo)
+	// foo = 1
+	fmt.Println("foo: ", foo)
+}
---
+++ ex2/declare.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const pi = 3.14
+	var foo int
+	bar := "foo bar baz quux"
+	baz := false
+
+	fmt.Printf("pi: %v\nfoo: %v\nbar: %v\nbaz: %v\n",
+	pi, foo, bar, baz)
+}
